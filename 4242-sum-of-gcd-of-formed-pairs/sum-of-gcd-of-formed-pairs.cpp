2ac5b24bc3eaa1ac62aa25612651d0935f8def8f
Arman hansda <155002128+armannhansda@users.noreply.github.com>
2026-07-16 11:50:51 +0530

    Time: 67 ms (85.87%) | Memory: 148.8 MB (97.39%) - LeetSync

--- 4242-sum-of-gcd-of-formed-pairs/sum-of-gcd-of-formed-pairs.cpp
+++ 4242-sum-of-gcd-of-formed-pairs/sum-of-gcd-of-formed-pairs.cpp
@@ -2,18 +2,24 @@ class Solution {
 public:
     long long gcdSum(vector<int>& nums) {
         
-        vector<int> prefixGcd;
+        // vector<int> prefixGcd;
         int maxele = nums[0];
         for(int i =0; i <nums.size(); i++){
             maxele = max(maxele, nums[i]);
-            prefixGcd.push_back(gcd(nums[i], maxele));
+            // prefixGcd.push_back(gcd(nums[i], maxele));
+
+            //in-place
+            nums[i] = gcd(nums[i], maxele);
         }
 
-        int i = 0, j = prefixGcd.size() -1;
+        // sort(prefixGcd.begin(), prefixGcd.end());
+        sort(nums.begin(), nums.end());
+        int i = 0, j = nums.size() -1;
+        
         long long sum = 0;
-        sort(prefixGcd.begin(), prefixGcd.end());
+
         while(i<j){
-            sum += gcd(prefixGcd[i], prefixGcd[j]);
+            sum += gcd(nums[i], nums[j]);
             i++;
             j--;
         }
